a52db3a416a2675ab81b68b1429458c6597b13ed
jleviaguirre <github@kamarble.com>
2021-06-06 00:49:39 -0600

    down arrow for services

--- kamarble.js
+++ kamarble.js
@@ -11,7 +11,7 @@ render = data => {
   });
   
   new WholePageSlider({
-    colors: ['black', 'deepskyblue', 'orange', 'lightgray', 'lightblue',
+    colors: ['#000', 'deepskyblue', 'orange', 'lightgray', 'lightblue',
     "#63b598", "#ce7d78", "#ea9e70", "#a48a9e", "#c6e1e8", "#648177" ,"#0d5ac1" ,
     "#f205e6" ,"#1c0365" ,"#14a9ad" ,"#4ca2f9" ,"#a4e43f" ,"#d298e2" ,"#6119d0",
     "#d2737d" ,"#c0a43c" ,"#f2510e" ,"#651be6" ,"#79806e" ,"#61da5e" ,"#cd2f00" ,
